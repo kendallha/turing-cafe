6d8044c5b705fc4ac97c70a47ce027da45b0bfe5
kendallha <kendallhaw@gmail.com>
2021-05-26 09:22:11 -0700

    Add handleChange method to Form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -11,13 +11,41 @@ class Form extends Component {
     }
   }
 
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
     return (
       <form>
-      <input type='text' placeholder='Name' name='name' value={this.state.name}></input>
-      <input type='text' placeholder='Date (mm/dd)' name='date' value={this.state.date}></input>
-      <input type='text' placeholder='Time' name='time' value={this.state.time}></input>
-      <input type='text' placeholder='Number of guests' name='number' value={this.state.number}></input>
+      <input 
+        type='text' 
+        placeholder='Name' 
+        name='name' 
+        value={this.state.name}
+        onChange={e => this.handleChange(e)}
+      />
+      <input 
+        type='text' 
+        placeholder='Date (mm/dd)' 
+        name='date' 
+        value={this.state.date}
+        onChange={e => this.handleChange(e)}
+      />
+      <input 
+        type='text' 
+        placeholder='Time' 
+        name='time' 
+        value={this.state.time}
+        onChange={e => this.handleChange(e)}
+      />
+      <input 
+        type='text' 
+        placeholder='Number of guests' 
+        name='number' 
+        value={this.state.number}
+        onChange={e => this.handleChange(e)}
+      />
       <button type='submit'>Make Reservation</button>
       </form>
     )
