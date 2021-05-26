a0e22f6fe2d75b00d95e13484ff215ea3feab152
kendallha <kendallhaw@gmail.com>
2021-05-26 09:08:04 -0700

    Add Form component and render in App component

---
+++ src/Form/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: null
+    }
+  }
+}
+
+export default Form;
